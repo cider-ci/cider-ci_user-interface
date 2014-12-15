a627cfacb55f3f167dab266f42ec713add6303f0
Max F. Albrecht <1@178.is>
2014-12-15 11:44:04 +0100

    UI fixes
    Squashed commit of the following:
    
    commit 2f98c87bc7511da78a10723d971e08e0d98632ef
    Author: Max F. Albrecht <1@178.is>
    Date:   Sun Dec 14 01:55:02 2014 +0100
    
        sass: executions-table: tone down the background colors, add top border
    
    commit e758d6e6872826b49417ba5cea41064a0e9eb092
    Author: Max F. Albrecht <1@178.is>
    Date:   Sun Dec 14 03:17:41 2014 +0100
    
        ui: navbar: show version next to brand IF it's a pre-release
    
    commit 9c33d7b1816e4e447bb74baaf0b5a654f5b9c9f3
    Author: Max F. Albrecht <1@178.is>
    Date:   Sun Dec 14 03:16:54 2014 +0100
    
        haml: add partial 'semantic_version'
    
    commit a8230d9dcbe468f01ed408ed239e59514ef935fb
    Author: Max F. Albrecht <1@178.is>
    Date:   Sun Dec 14 03:16:01 2014 +0100
    
        rails env: Set Haml output to ugly everywhere
    
    commit 63217337f0b3976fb040c873bb1aabaac959c2e8
    Author: Max F. Albrecht <1@178.is>
    Date:   Sun Dec 14 01:53:51 2014 +0100
    
        sass: make colors_and_states themeable
    
    commit b27617328efedbbfe086cbda3aa6745fd64bdf96
    Author: Max F. Albrecht <1@178.is>
    Date:   Sun Dec 14 01:53:36 2014 +0100
    
        sass: small visual fixes
    
    commit 8d027361f0ff1376583726a0b46b001e0df20481
    Author: Max F. Albrecht <1@178.is>
    Date:   Sun Dec 14 01:51:39 2014 +0100
    
        views: workspace/executions: remove tr.spacer from table
    
    commit 4714a4dd78177a2c69c834465bb2757f2d4e267c
    Author: Max F. Albrecht <1@178.is>
    Date:   Sun Dec 14 01:51:03 2014 +0100
    
        haml: remove whitespace from inside .label

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application.
 require File.expand_path('../application', __FILE__)
 
+# Set Haml output to ugly everywhere
+Haml::Template.options[:ugly] = true
+
 # Initialize the rails application.
 CiderCI::Application.initialize!
