70f2937ba8543b8ef799ae09528fb4d5b88d8e90
Thomas Schank <DrTom@schank.ch>
2015-11-12 20:24:05 +0100

    Adjust db/seeds and sync with dev/setup documentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,14 @@
 User.find_or_initialize_by(login: 'admin')  \
   .update_attributes! is_admin: true, password: 'secret'
 
-
-Repository.find_or_initialize_by(name: 'Demo Project') \
+Repository.find_or_initialize_by(name: 'Cider-CI Bash Demo Project') \
   .update_attributes!(
-    git_url: 'http://localhost:8888/cider-ci/demo-project-bash/',
+    git_url: Rails.root.join("..","demo-project-bash").to_s,
     git_fetch_and_update_interval: 5,
     public_view_permission: true)
 
 
-Executor.find_or_initialize_by(name: "DemoExecutor") \
-  .update_attributes!(base_url: "http://localhost:8883")
+Executor.find_or_initialize_by(name: "DemoExecutor",
+   id: "35cff40c-b4f8-4ca3-9217-d49c9c35f375") \
+ .update_attributes!(base_url: 'http://localhost:8883')
 
