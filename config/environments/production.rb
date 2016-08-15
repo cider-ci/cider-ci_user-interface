49f7181ac31a2f87ed4bfb15bfe92ed77d903e40
Thomas Schank <DrTom@schank.ch>
2016-08-15 21:31:57 +0200

    Honor RAILS_LOG_LEVEL in production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ CiderCI::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :warn
+  config.log_level = ENV['RAILS_LOG_LEVEL'].present? ? ENV['RAILS_LOG_LEVEL'] : :warn
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
