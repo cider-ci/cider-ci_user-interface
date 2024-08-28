8daf1cf97009266a227e0ed9c8ef1173de1f9ce7
Matus Kmit <matuskmit1@gmail.com>
2024-08-28 13:34:34 +0200

    after running app:update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
-require 'active_support/core_ext/numeric/bytes'
+require "active_support/core_ext/integer/time"
+require "active_support/core_ext/numeric/bytes"
 
 CiderCI::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -39,7 +40,7 @@ CiderCI::Application.configure do
   config.assets.digest = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  # config.asset_host = 'http://assets.example.com'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
@@ -56,8 +57,8 @@ CiderCI::Application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
-  # Use the lowest log level to ensure availability of diagnostic information
-  # when problems arise.
+  # Include generic and useful information about system operation, but avoid logging too much
+  # information to avoid inadvertent exposure of personally identifiable information (PII).
   config.log_level = ENV['RAILS_LOG_LEVEL'].present? ? ENV['RAILS_LOG_LEVEL'] : :warn
 
   # Prepend all log lines with the following tags.
@@ -67,7 +68,7 @@ CiderCI::Application.configure do
   # config.cache_store = :mem_cache_store
   config.cache_store = :memory_store, {size: ((ENV['CACHE_STORE_SIZE_MB'].presence.try(&:to_i) || 128 ).megabytes rescue 128.megabytes)}
 
-  # Use a real queuing backend for Active Job (and separate queues per environment)
+  # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "cider_ci_production"
 
@@ -84,11 +85,17 @@ CiderCI::Application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  # Log disallowed deprecations.
+  config.active_support.disallowed_deprecation = :log
+
+  # Tell Active Support which deprecation messages to disallow.
+  config.active_support.disallowed_deprecation_warnings = []
+
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
   # Use a different logger for distributed setups.
-  # require 'syslog/logger'
+  # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 CiderCI::Application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/workspace', controller: 'workspace', action: 'index', as: 'workspace'
 
